4806c5fabac8ffe35556964484105e7865eadf69
Miguel C <webdazer0@gmail.com>
2021-05-16 20:49:49 +0200

    Only Async/await, removed all promises .then

--- controller/quizCtrl.js
+++ controller/quizCtrl.js
@@ -1,23 +1,36 @@
 const Quiz = require("../model/Quiz");
 const path = require("path");
 
-const getAll = (req, res) => {
-  Quiz.find()
-    .then((allQuiz) => res.json(allQuiz))
-    .catch((err) => res.status(400).json("Error: " + err));
+const getAll = async (req, res) => {
+  try {
+    const allQuiz = await Quiz.find();
+    return res.status(200).json(allQuiz);
+  } catch (err) {
+    res.status(400).json("Error: " + err.message);
+  }
 };
 
 const create = async (req, res) => {
-  const { question, answers, correct_answer } = req.body;
-  const newQuiz = new Quiz({ question, answers, correct_answer });
-  console.log(newQuiz);
-  await newQuiz.save();
-  res.json({ message: "Question X Saved" });
+  try {
+    const { question, answers, correct_answer } = req.body;
+    const newQuiz = new Quiz({ question, answers, correct_answer });
+    console.log(newQuiz);
+    await newQuiz.save();
+    return res.status(200).json({ message: "Question X Saved" });
+  } catch (error) {
+    // console.log(error.message);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 const deleteById = async (req, res) => {
-  const quiz = await Quiz.findByIdAndDelete(req.params.id);
-  res.json({ message: "Question Deleted" });
+  try {
+    const quiz = await Quiz.findByIdAndDelete(req.params.id);
+    return res.status(200).json({ message: "Question Deleted" });
+  } catch (error) {
+    // console.log(error.message);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 module.exports = { getAll, create, deleteById };
--- db.js
+++ db.js
@@ -2,12 +2,19 @@ const mongoose = require("mongoose");
 
 const URI_DB = "mongodb://localhost/db_quiz";
 
-mongoose
-  .connect(URI_DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then((db) => console.log(`DB is connected`))
-  .catch((err) => console.error(err));
+const optionsDB = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+};
+
+// IIFE (Immediately Invoked Function Expression) to use Async/Await
+(async () => {
+  try {
+    await mongoose.connect(URI_DB, optionsDB);
+    console.log(`DB is connected`);
+  } catch (err) {
+    console.error(err);
+  }
+})();
