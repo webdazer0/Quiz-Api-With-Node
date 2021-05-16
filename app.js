c1f439367be02505fd7895d47049067a50d9a7a8
Miguel C <webdazer0@gmail.com>
2021-05-16 20:29:21 +0200

    improved code

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require("express");
-const router = require("./routes");
 const morgan = require("morgan");
 const cors = require("cors");
 
 const app = express();
 
 require("./db");
-app.listen(5000, () => {
-  console.log("Server on port :", 5000);
-});
 
 // Middlewares
 app.use(morgan("dev"));
@@ -17,4 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // routes
-app.use("/api/quiz", require("./routes"));
+app.use("/api/quiz", require("./routes/quiz.routes"));
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 const app = require("./app");
+
+app.listen(4500, () => console.log("Server on port :", 4500));
